01a8b7b4b37dcd831497b03795ce337f74a65674
Abid Ahmad <abid.programming@gmail.com>
2020-05-11 17:47:44 +0530

    Fixed Response

--- run.py
+++ run.py
@@ -12,7 +12,8 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-mapped_value_to_food = ['Poha', 'Sabudana', 'Samosa']
+mapped_value_to_food = ['Poha', 'SabudanaKhichdi', 'Samosa']
+
 
 @app.route('/home', methods=['POST'])
 def home():
@@ -20,7 +21,7 @@ def home():
     data.save(secure_filename(data.filename))
     img_path = data.filename
     new_image = load_image(img_path, True)
-  
+
     with sess.as_default():
         with graph.as_default():
             predicted_food = model.predict_classes(new_image)
@@ -28,20 +29,34 @@ def home():
             print(predicted_food)
             print(predicted_probablities)
             print(mapped_value_to_food[predicted_food[0]])
- 
-    return jsonify({"status":"ok"})
+            resp = {"Predicted-Class": int(predicted_food[0]),
+                    "Predicted-Food": mapped_value_to_food[predicted_food[0]],
+                    "Probablities": [ 
+                                    { 
+                                        mapped_value_to_food[0] : float(predicted_probablities[0][0]),
+                                        mapped_value_to_food[1] : float(predicted_probablities[0][1]),
+                                        mapped_value_to_food[2] : float(predicted_probablities[0][2])
+                                    }
+                                ]
+                    }
+    return jsonify(resp), 200
+
 
 def load_image(img_path, show=False):
-    img = image.load_img(img_path, target_size=(224,224))
-    img_tensor = image.img_to_array(img)                    # (height, width, channels)
-    img_tensor = np.expand_dims(img_tensor, axis=0)         # (1, height, width, channels), add a dimension because the model expects this shape: (batch_size, height, width, channels)
-    img_tensor /= 255.                                      # imshow expects values in the range [0, 1]
+    img = image.load_img(img_path, target_size=(224, 224))
+    # (height, width, channels)
+    img_tensor = image.img_to_array(img)
+    # (1, height, width, channels), add a dimension because the model expects this shape: (batch_size, height, width, channels)
+    img_tensor = np.expand_dims(img_tensor, axis=0)
+    # imshow expects values in the range [0, 1]
+    img_tensor /= 255.
     return img_tensor
+
 
 if __name__ == "__main__":
     sess = tf.Session()
     graph = tf.get_default_graph()
     set_session(sess)
     model = load_model('projectmodel.h5')
-        
-    app.run(debug=True)
+
+    app.run(host='0.0.0.0')
